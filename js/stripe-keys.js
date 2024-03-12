c2c81e0dc7b564b9440b06592ddd859826362c2c
Danilo <dyarce@uc.edu.co>
2024-03-12 06:18:41 -0500

    Se suben archivos del proyecto kalo

---
+++ js/stripe-keys.js
@@ -0,0 +1,4 @@
+export default {
+    public_key: 'pk_test_51OsPnQFRA9hwBJho46bndg6kcGocwIzmlUG5TFIiW1jrO2Eoah5VxxUaZNo1ABr1LKX64uWOiAbtDXxGzasKDXM000ix6AcndJ',
+    private_key: 'sk_test_51OsPnQFRA9hwBJholOIwRlnwYJEKUtlNYQXKkdx36VbZJqIcDxcG6bXgQhEzW7z2c1pfsxtWZjR3I6L2AvvfWOjv00htHmLQhk'
+}
